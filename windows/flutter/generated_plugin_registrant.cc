945ff8e707a494d86530c9e5f33d260f05475c4b
edusoftdevelopment <75130896+edusoftdevelopment@users.noreply.github.com>
2025-08-21 09:25:41 +0500

    app config some changing

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <mssql_connection/mssql_connection_plugin_c_api.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  MssqlConnectionPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MssqlConnectionPluginCApi"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
 }
