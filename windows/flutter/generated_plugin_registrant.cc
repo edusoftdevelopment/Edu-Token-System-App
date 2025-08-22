36a375dfedaa0ec9b9f9ff64c1bb8b7c739636c7
edusoftdevelopment <75130896+edusoftdevelopment@users.noreply.github.com>
2025-08-22 12:02:34 +0500

    some minor changing Blue tooth printer class

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -8,10 +8,13 @@
 
 #include <mssql_connection/mssql_connection_plugin_c_api.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
+#include <print_bluetooth_thermal/print_bluetooth_thermal_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   MssqlConnectionPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("MssqlConnectionPluginCApi"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+  PrintBluetoothThermalPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PrintBluetoothThermalPluginCApi"));
 }
