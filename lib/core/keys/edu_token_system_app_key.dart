dea27dbc237767e81b5bda74610c178caf487f95
edusoftdevelopment <edusoftdevelopment@gmail.com>
2025-08-25 15:26:30 +0500

    ..

--- lib/core/keys/edu_token_system_app_key.dart
+++ lib/core/keys/edu_token_system_app_key.dart
@@ -1,2 +1,6 @@
 // ! Dot Environment Path
 String get dotEnvPath => 'assets/dotenv/.env';
+
+// ! Shared Preferences Keys
+String get savedPasswordKey => 'saved_password';
+String get currentDatabseKey => 'selectedDb';
